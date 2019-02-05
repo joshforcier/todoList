47b5087840b32d14194a55049cb56b1a166f56c0
Josh Forcier <josh.forcier@tgbbrands.com>
2019-02-05 15:00:25 -0600

    init commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="test">
+        <div v-bind:keey="todo.id" v-for="todo in todos">
+            <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"></TodoItem>
+        </div>
+    </div>
+</template>
+
+<script>
+    import TodoItem from './TodoItem';
+    export default  {
+        name: "Todos",
+        props: ["todos"],
+        components: {
+            TodoItem
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="about">
+        <h1>About</h1>
+        <p>TodoList app.</p>
+    </div>
+</template>
